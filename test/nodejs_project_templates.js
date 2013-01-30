b518ecf22ce0702aa0fe56a4791b235cdb3a9186
sethyuan <cavingdeep@gmail.com>
2013-01-30 23:36:19 +0800

    Templates loading implemented.

--- test/nodejs_project_templates.js
+++ test/nodejs_project_templates.js
@@ -1,21 +1,38 @@
 var fgen = require("../"),
+    should = require("should"),
     fs = require("fs.extra");
 
 describe("fgen", function() {
   it("create generator should be ok", function() {
-    fgen.createGenerator("./nodejs_project_templates/").should.be.ok;
+    fgen.createGenerator("test/nodejs_project_templates/").should.be.ok;
   });
 
   it("ready event is fired (within 1s)", function(done) {
-    this.timeout(1000);
-    fgen.createGenerator("./nodejs_project_templates/", done);
+    this.timeout(500);
+    fgen.createGenerator("test/nodejs_project_templates/", done);
+  });
+
+  it("ready event fired with templates compiled", function(done) {
+    this.timeout(500);
+    fgen.createGenerator("test/nodejs_project_templates/", function(err, gen) {
+      should.not.exist(err);
+      gen.should.have.property("templates").with.keys(
+        "LICENSE",
+        "README.md",
+        "package.json",
+        ".gitignore",
+        ".npmignore",
+        "lib/__name__.js");
+      done();
+    });
   });
 
   describe("generator", function() {
     var gen;
 
     before(function(done) {
-      gen = fgen.createGenerator("./nodejs_project_templates/", function() {
+      gen = fgen.createGenerator("test/nodejs_project_templates/", function(err) {
+        should.not.exist(err);
         gen.context = {
           name: "testlib",
           version: "0.0.0",
@@ -25,53 +42,53 @@ describe("fgen", function() {
       });
     });
 
-    afterEach(function() {
-      fs.rmrf("./nodejs_project_templates/tmp", function(err) {
+    afterEach(function(done) {
+      fs.rmrf("test/nodejs_project_templates/tmp", function(err) {
         err && console.log(err);
         done();
       });
     });
 
     it("all files generation to folder should be ok", function(done) {
-      gen.generateAll("./nodejs_project_templates/tmp", function(err) {
+      gen.generateAll("test/nodejs_project_templates/tmp", function(err) {
         should.not.exist(err);
-        fs.existsSync("./nodejs_project_templates/tmp/LICENSE").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/README.md").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/package.json").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/lib").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/test").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/.gitignore").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/.npmignore").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/lib/testlib.js").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/LICENSE").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/README.md").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/package.json").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/lib").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/test").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/.gitignore").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/.npmignore").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/lib/testlib.js").should.be.true;
         done();
       });
     });
 
     it("all files generation to folder with '/' should be ok", function(done) {
-      gen.generateAll("./nodejs_project_templates/tmp/", function() {
+      gen.generateAll("test/nodejs_project_templates/tmp/", function() {
         should.not.exist(err);
-        fs.existsSync("./nodejs_project_templates/tmp/LICENSE").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/README.md").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/package.json").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/lib").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/test").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/.gitignore").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/.npmignore").should.be.true;
-        fs.existsSync("./nodejs_project_templates/tmp/lib/testlib.js").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/LICENSE").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/README.md").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/package.json").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/lib").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/test").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/.gitignore").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/.npmignore").should.be.true;
+        fs.existsSync("test/nodejs_project_templates/tmp/lib/testlib.js").should.be.true;
         done();
       });
     });
 
     it("single file to folder should be generated", function(done) {
-      gen.generate("LICENSE", "./nodejs_project_templates/tmp/", function() {
-        fs.existsSync("./nodejs_project_templates/tmp/LICENSE").should.be.true;
+      gen.generate("LICENSE", "test/nodejs_project_templates/tmp/", function() {
+        fs.existsSync("test/nodejs_project_templates/tmp/LICENSE").should.be.true;
         done();
       });
     });
 
     it("single file to file should be generated", function(done) {
-      gen.generate("LICENSE", "./nodejs_project_templates/tmp/LIC", function() {
-        fs.existsSync("./nodejs_project_templates/tmp/LIC").should.be.true;
+      gen.generate("LICENSE", "test/nodejs_project_templates/tmp/LIC", function() {
+        fs.existsSync("test/nodejs_project_templates/tmp/LIC").should.be.true;
         done();
       });
     });
